48c185f1ad61dc99433be67fe1ab2bd825571839
jpriestley4 <119522891+jpriestley4@users.noreply.github.com>
2023-02-17 12:24:22 +0000

    commit changes

---
+++ tulip/tulip.js
@@ -0,0 +1,75 @@
+// dynamic page navigation
+
+document.querySelectorAll(".local-nav-link").forEach(anchor => {
+    anchor.addEventListener("click", function(e){
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior:"smooth"
+      });
+    });
+  });
+
+// Nav Scrolling Effect
+
+const li=document.querySelectorAll(".local-nav-link");
+const sec=document.querySelectorAll("section");
+
+function activeMenu(){
+  let len=sec.length;
+  while(--len && window.scrollY +2 < sec[len].offsetTop){}
+  li.forEach(ltx => ltx.classList.remove("active"));
+  li[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll", activeMenu)
+
+// filter effect
+
+// show/hide typography details on button click
+
+// add a listener to buttons 
+var topicButtonElements = document.querySelectorAll(".topic-button");
+topicButtonElements.forEach(addTopicButtonListener);
+
+function addTopicButtonListener(topicButtonElement){
+  topicButtonElement.addEventListener("click",topicButtonClick);
+}
+
+function topicButtonClick(event){
+var clickedButton = event.currentTarget;
+
+// Capture first word of clicked button to remove spaces from css selector
+let myStr = clickedButton.textContent
+let firstWord = myStr.split(" ")[0].toLowerCase()
+console.log(firstWord)
+
+// generate the class of the selected topics's details element
+var detailsElementCssSelector=".topic-details-" + firstWord
+console.log(detailsElementCssSelector)
+
+// find this element
+var detailsElement = document.querySelector(detailsElementCssSelector);
+console.log(detailsElement)
+
+// remove selected state from all buttons
+topicButtonElements.forEach(updateClickedButtonState);
+
+// remove selected state from all details elements
+var topicDetailsElements = document.querySelectorAll(".topic-details");
+topicDetailsElements.forEach(updateTopicDetailState);
+
+// add selected state just to the clicked button
+detailsElement.classList.replace("hide","show");
+clickedButton.classList.add("is-success");
+}
+
+function updateClickedButtonState(topicButtonElement){
+  topicButtonElement.classList.replace("is-success","is-primary");
+}
+
+function updateTopicDetailState(topicDetailElement){
+  topicDetailElement.classList.add("hide");
+}
+
+// scroll btn
+
